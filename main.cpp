7a7638dc729f84001717fe56019f654ca16e27bd
NISHITA <nishitaietmca05@gmail.com>
2026-01-08 21:38:27 +0530

    created some some functions to update and delete employee record

--- main.cpp
+++ main.cpp
@@ -42,6 +42,7 @@ void addEmployee(){
     cin>>e.empID;
 
     cout<<"Enter the name of employee: "<<endl;
+    cin.ignore();
     getline(cin, e.name);
 
     cout<<"Enter the base salary: "<<endl;
@@ -51,7 +52,7 @@ void addEmployee(){
 
     //writing data into file
 
-    file << e.empID <<" | " << e.name << " | " <<e.baseSalary << " | " << e.HRA << " | " <<e.DA << " | " << e.grossSalary << " | " <<e.tax <<endl;
+    file << e.empID << " "<< e.name << " "<< e.baseSalary << " "<< e.HRA << " "<< e.DA << " "<< e.grossSalary << " "<< e.tax << endl;
 
     file.close();
     cout<<"Employee added successfully" <<endl;
@@ -60,24 +61,22 @@ void addEmployee(){
 
 //search any employee by ID
 
-void searchEmployee(){
-
+void searchEmployee() {
     Employee e;
     int id;
     bool found = false;
 
-    cout << "Enter Employee's ID you want to search: ";
+    cout << "\nEnter Employee ID to search: ";
     cin >> id;
 
-    ifstream file("employee.txt"); //reading data from file
+    ifstream file("employee.txt");
 
     while (file >> e.empID >> e.name >> e.baseSalary >> e.HRA >> e.DA >> e.grossSalary >> e.tax) {
         if (e.empID == id) {
-            cout << "Employee Found!";
+            cout << "Employee Found!"<<endl;
             cout << "ID: " << e.empID << endl;
             cout << "Name: " << e.name << endl;
             cout << "Base Salary: " << e.baseSalary << endl;
-
             found = true;
             break;
         }
@@ -86,8 +85,7 @@ void searchEmployee(){
     file.close();
 
     if (!found)
-        cout << "Employee not found!"<<endl;
-
+        cout << "Employee not found!";
 }
 
 void showSalarySlip() {
@@ -102,7 +100,7 @@ void showSalarySlip() {
 
     while (file >> e.empID >> e.name >> e.baseSalary >> e.HRA >> e.DA >> e.grossSalary >> e.tax) {
         if (e.empID == id) {
-            cout << "------------- Salary Slip ------------";
+            cout << "------------- Salary Slip ------------"<<endl;
             cout << fixed << setprecision(2);
             cout << "Employee ID   : " << e.empID << endl;
             cout << "Name          : " << e.name << endl;
@@ -121,9 +119,123 @@ void showSalarySlip() {
     file.close();
 
     if (!found)
-        cout << "Employee not found!";
+        cout << "Employee not found!"<<endl;
+}
+
+//update employee data using a temporary file 
+
+void updateEmployee() {
+    Employee e;
+    int id;
+    bool found = false;
+
+    cout << "Enter Employee ID to update: ";
+    cin >> id;
+
+    ifstream file("employee.txt");
+    ofstream temp("temp.txt");
+
+    while (file >> e.empID >> e.name >> e.baseSalary >> e.HRA >> e.DA >> e.grossSalary >> e.tax) {
+        if (e.empID == id) {
+            cout << "Enter new name: ";
+            cin >> e.name;
+
+            cout << "Enter new basic salary: ";
+            cin >> e.baseSalary;
+
+            calculateSalary(e);
+            found = true;
+        }
+
+        temp << e.empID << " "
+             << e.name << " "
+             << e.baseSalary << " "
+             << e.HRA << " "
+             << e.DA << " "
+             << e.grossSalary << " "
+             << e.tax << endl;
+    }
+
+    file.close();
+    temp.close();
+
+    remove("employee.txt");
+    rename("temp.txt", "employee.txt");
+
+    if (found)
+        cout << "Employee updated successfully!"<<endl;
+    else
+        cout << "Employee not found!"<<endl;
+}
+
+void deleteEmployee() {
+    Employee e;
+    int id;
+    bool found = false;
+
+    cout << "Enter Employee ID to delete: ";
+    cin >> id;
+
+    ifstream file("employee.txt");
+    ofstream temp("temp.txt");
+
+    while (file >> e.empID >> e.name >> e.baseSalary >> e.HRA >> e.DA >> e.grossSalary >> e.tax) {
+        if (e.empID != id) {
+            temp << e.empID << " "
+                 << e.name << " "
+                 << e.baseSalary << " "
+                 << e.HRA << " "
+                 << e.DA << " "
+                 << e.grossSalary << " "
+                 << e.tax << endl;
+        } else {
+            found = true;
+        }
+    }
+
+    file.close();
+    temp.close();
+
+    remove("employee.txt");
+    rename("temp.txt", "employee.txt");
+
+    if (found)
+        cout << "Employee deleted successfully!"<<endl;
+    else
+        cout << "Employee not found!"<<endl;
 }
 
 int main(){
 
+int choice;
+
+    do {
+        cout << "---------- Employee Salary Management System ----------"<<endl;
+        cout << "1. Add Employee"<<endl;
+        cout << "2. Search Employee"<<endl;
+        cout << "3. Update Employee data"<<endl;
+        cout << "4. Delete Employee"<<endl;
+        cout << "5. Show Salary Slip"<<endl;
+        cout << "6. Exit"<<endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: addEmployee(); 
+            break;
+            case 2: searchEmployee(); 
+            break;
+            case 3: updateEmployee(); 
+            break;
+            case 4: deleteEmployee(); 
+            break;
+            case 5: showSalarySlip(); 
+            break;
+            case 6: cout << "Exiting"; 
+            break;
+            default: cout << "Invalid choice!"<<endl;
+        }
+    } while (choice != 6);
+
+    return 0;
 }
