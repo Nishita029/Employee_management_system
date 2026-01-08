a84f946e9c146f1ee7e52219da9dc0dc37ef9ae5
NISHITA <nishitaietmca05@gmail.com>
2026-01-08 16:14:48 +0530

    added one more feature of displaying salary slip

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<iomanip>
 
 using namespace std;
 
@@ -35,19 +36,92 @@ void addEmployee(){
     Employee e;
 
     ofstream file;
-    file.open("employee.txt", ios::app);
+    file.open("employee.txt", ios::app); //opening file and if it does not exist it will create a new file
 
     cout<<"Enter employee ID: "<<endl;
     cin>>e.empID;
 
     cout<<"Enter the name of employee: "<<endl;
-    cin>>e.name;
+    getline(cin, e.name);
 
     cout<<"Enter the base salary: "<<endl;
     cin>>e.baseSalary;
 
     calculateSalary(e);
+
+    //writing data into file
+
+    file << e.empID <<" | " << e.name << " | " <<e.baseSalary << " | " << e.HRA << " | " <<e.DA << " | " << e.grossSalary << " | " <<e.tax <<endl;
+
+    file.close();
+    cout<<"Employee added successfully" <<endl;
      
+}
+
+//search any employee by ID
+
+void searchEmployee(){
+
+    Employee e;
+    int id;
+    bool found = false;
+
+    cout << "Enter Employee's ID you want to search: ";
+    cin >> id;
+
+    ifstream file("employee.txt"); //reading data from file
+
+    while (file >> e.empID >> e.name >> e.baseSalary >> e.HRA >> e.DA >> e.grossSalary >> e.tax) {
+        if (e.empID == id) {
+            cout << "Employee Found!";
+            cout << "ID: " << e.empID << endl;
+            cout << "Name: " << e.name << endl;
+            cout << "Base Salary: " << e.baseSalary << endl;
+
+            found = true;
+            break;
+        }
+    }
+
+    file.close();
+
+    if (!found)
+        cout << "Employee not found!"<<endl;
+
+}
+
+void showSalarySlip() {
+    Employee e;
+    int id;
+    bool found = false;
+
+    cout << "Enter Employee ID: ";
+    cin >> id;
+
+    ifstream file("employee.txt");
+
+    while (file >> e.empID >> e.name >> e.baseSalary >> e.HRA >> e.DA >> e.grossSalary >> e.tax) {
+        if (e.empID == id) {
+            cout << "------------- Salary Slip ------------";
+            cout << fixed << setprecision(2);
+            cout << "Employee ID   : " << e.empID << endl;
+            cout << "Name          : " << e.name << endl;
+            cout << "Base Salary  : " << e.baseSalary << endl;
+            cout << "HRA (20%)     : " << e.HRA << endl;
+            cout << "DA (10%)      : " << e.DA << endl;
+            cout << "Gross Salary  : " << e.grossSalary << endl;
+            cout << "Tax (5%)      : " << e.tax << endl;
+            cout << "Net Salary    : " << e.grossSalary - e.tax << endl;
+        
+            found = true;
+            break;
+        }
+    }
+
+    file.close();
+
+    if (!found)
+        cout << "Employee not found!";
 }
 
 int main(){
