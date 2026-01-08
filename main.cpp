87fdfc9b64e4a425d47815cbb67a2d3659989723
NISHITA <nishitaietmca05@gmail.com>
2026-01-08 15:08:56 +0530

    created a structure of employee to store info

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+struct Employee{
+    int empID;
+    string name;
+    float baseSalary;
+    float HRA;
+    float DA;
+    float grossSalary;
+    float tax;
+};
+
+
+int main(){
+
+}
