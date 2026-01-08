ac36329dc9a54a4a463eb0e071458b27e5cb3186
NISHITA <nishitaietmca05@gmail.com>
2026-01-08 15:27:23 +0530

    created the fuction to calculate salary and add new employee

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+//Structure of employee to store information
+
 struct Employee{
     int empID;
     string name;
@@ -14,6 +16,39 @@ struct Employee{
     float tax;
 };
 
+//fuction to calculate salary 
+
+void calculateSalary(Employee &e){
+
+    e.HRA = 0.20 * e.baseSalary;
+    e.DA = 0.10 * e.baseSalary;
+
+    e.grossSalary = e.baseSalary + e.HRA + e.DA ;
+
+    e.tax = 0.05 * e.grossSalary;
+
+}
+
+// function to add new record of employee
+
+void addEmployee(){
+    Employee e;
+
+    ofstream file;
+    file.open("employee.txt", ios::app);
+
+    cout<<"Enter employee ID: "<<endl;
+    cin>>e.empID;
+
+    cout<<"Enter the name of employee: "<<endl;
+    cin>>e.name;
+
+    cout<<"Enter the base salary: "<<endl;
+    cin>>e.baseSalary;
+
+    calculateSalary(e);
+     
+}
 
 int main(){
 
